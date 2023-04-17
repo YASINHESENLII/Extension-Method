b99e504220a0e8519af48d4374aead08dc9b45d0
YASINHESENLII <yasinhesenli44@gmail.com>
2023-04-18 00:52:08 +0400

    Static

---
+++ Extention method/Helper.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Extention_method
+{
+    internal static class Helper
+    {
+        public static bool IsOdd(this int number)
+        {
+            if (number % 2 == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+
+            }
+        }
+        public static bool IsEven(this int number)
+        {
+            if (number % 2 != 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+
+            }
+        }
+        public static bool Hasdigit(this string text)
+        {
+            foreach (char c in text)
+            {
+                if (char.IsDigit(c))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool CheckPassword(this string password)
+        {
+            if (password.Length <8)
+            {
+                return false;
+            }
+            foreach (char c in password)
+            {
+                if (!char.IsDigit(c))
+                {
+                    return true;
+                }
+                else if (char.IsLower(c))
+                {
+                    return true;
+                }
+                else if (!char.IsUpper(c))
+                {
+                    return true;
+                }
+
+            }
+
+            return false;
+            //   return password.Any(char.IsUpper) && password.Any(char.IsLower) && password.Any(char.IsDigit) && password.Length <= 8;
+        }
+
+
+        public static string Capitalize(this string input)
+        {
+           
+            return char.ToUpper(input[0]) + input.Substring(1).ToLower();
+
+
+        }
+    }
+   
+
+    }
---
+++ Extention method/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Extention_method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int myNumber = 5;
+            bool isOdd = myNumber.IsOdd();
+            bool isEven = myNumber.IsEven();
+
+            string myText = "32!";
+            bool hasDigit = myText.Hasdigit();
+            string capitalizedText = myText.Capitalize();
+
+            string myPassword = "pas";
+            bool isValidPassword = myPassword.CheckPassword();
+
+            Console.WriteLine(isOdd);
+            Console.WriteLine(isEven);
+            Console.WriteLine(  hasDigit);
+            Console.WriteLine(capitalizedText);
+            Console.WriteLine(isValidPassword);
+
+
+
+
+
+
+        }
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
